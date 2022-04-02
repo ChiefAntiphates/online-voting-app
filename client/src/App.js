a864bb90d969f36d75192d028b17e4b6655d1d74
Harry <hw00441@surrey.ac.uk>
2022-04-02 13:38:11 +0100

    Sockets with namespaces example code working (button clicking)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { io } from "socket.io-client";
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 
+const SOCKET_BASE = "http://localhost:5000"
 
 function App() {
 
+  const [buttonClicks, setButtonClicks] = useState(0)
 
   useEffect(() => {
-    const socket = io("http://localhost:5000");
-    socket.on("test_topic", data => {
+    const test_socket = io(`${SOCKET_BASE}/testnp`);
+    test_socket.on("test_topic", data => {
+      console.log(data) 
+    })
+    //socketio.emit('clicked', {"number": button_clicks}, namespace='/button_clicks')
+    const button_socket = io(`${SOCKET_BASE}/button_clicks`)
+    button_socket.on("clicked", data => {
+      console.log("HELLO")
       console.log(data)
-    });
+      setButtonClicks(data.number)
+    })
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>There have been {buttonClicks} button clicks</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
--- server/app.py
+++ server/app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
 
+button_clicks = 0
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
@@ -15,8 +17,15 @@ def hello_world():
 
 @app.route('/test')
 def my_test():
-    socketio.emit('test_topic', 'Success!!!')
+    socketio.emit('test_topic', 'Success!!!', namespace='/testnp')
     return jsonify(body='This works!'), 200
+
+@app.route('/click')
+def click():
+    global button_clicks
+    button_clicks += 1
+    socketio.emit('clicked', {"number": button_clicks}, namespace='/button_clicks')
+    return jsonify(body="You clicked the button", clicks=button_clicks)
 
 if __name__ == '__main__':
     app.debug=True
