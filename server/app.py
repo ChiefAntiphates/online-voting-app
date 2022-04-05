7f6b3e6c12aeb0fad7c6ebee7694f62d6ceab516
Harry <hw00441@surrey.ac.uk>
2022-04-05 22:14:28 +0100

    Added basic redis integration

--- server/app.py
+++ server/app.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
 from dotenv import load_dotenv
 import voting_utils as utils
 import os
+import redis
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+r = redis.Redis(host='localhost', port=6379)
 
-
-socketio = SocketIO(app, cors_allowed_origins=os.getenv('CLIENT_URL')or"http://localhost:3000")
+socketio = SocketIO(
+        app, 
+        cors_allowed_origins=os.getenv('CLIENT_URL')or"http://localhost:3000",
+        message_queue='redis://'
+    )
 
 button_clicks = 0
 
-print("loaded.")
+
 
 @app.route('/')
 def hello_world():
     print("I got it!")
+    r.publish("test1", "hello")
     return ("hello")
 
+@app.route('/set_name')
+def redis_set():
+    args = request.args.to_dict()
+    r.set('name', args['name'])
+    return(f"Set name to {args['name']}.")
 
 @app.route('/test')
 def my_test():
@@ -37,5 +48,5 @@ def click():
 
 if __name__ == '__main__':
     app.debug=True
-    socketio.run(app, host='0.0.0.0', port=os.getenv('SERVER_PORT') or 5000)
-
+    socketio.run(
+        app, host='0.0.0.0', port=os.getenv('SERVER_PORT') or 5000)
