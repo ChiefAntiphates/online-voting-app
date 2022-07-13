506b38f4d187511c57f6efe67744f639b0c17c86
Harry <hw00441@surrey.ac.uk>
2022-07-13 18:51:23 +0100

    Mid commit

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version  = ">= 1.2.5"
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = ">= 4.22.0"
+    }
+    random = {
+        source  = "hashicorp/random"
+        version = ">= 3.0"
+    }
+    cloudinit = {
+        source  = "hashicorp/cloudinit"
+        version = ">= 2.1"
+    }
+  }
+}
